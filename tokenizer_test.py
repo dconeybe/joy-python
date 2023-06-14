fabfd3f79879172ba15af66c46029f1f8432161f
Denver Coneybeare <dconeybe@google.com>
2023-06-14 17:08:31 -0400

    tokenizer_test.py: updated, but still has an error

--- tokenizer_test.py
+++ tokenizer_test.py
@@ -1,8 +1,8 @@
 import io
 
-import tokenizer
-
 from absl.testing import absltest
+
+import tokenizer
 
 
 class TokenizerTest(absltest.TestCase):
@@ -38,6 +38,60 @@ class TokenizerTest(absltest.TestCase):
             start_column_number=1,
             end_column_number=63,
             identifier=source_text,
+        ),
+    )
+
+  def test_yields_a_bunch_of_tokens_on_one_line(self):
+    source_text = "abc def ghi a12 b23 a_      b"
+    t = tokenizer.Tokenizer(tokenizer.SourceReader(io.StringIO(source_text)))
+    token = tokenizer.Token()
+
+    token1 = t.next()
+    token2 = t.next()
+    token3 = t.next()
+    token4 = t.next()
+    token5 = t.next()
+    token6 = t.next()
+    token_last = t.next()
+
+    self.assertEqual(
+        token1,
+        tokenizer.Token(
+            start_line_number=1,
+            end_line_number=1,
+            start_column_number=1,
+            end_column_number=3,
+            identifier="abc",
+        ),
+    )
+    self.assertEqual(
+        token2,
+        tokenizer.Token(
+            start_line_number=1,
+            end_line_number=1,
+            start_column_number=5,
+            end_column_number=7,
+            identifier="def",
+        ),
+    )
+    self.assertEqual(
+        token3,
+        tokenizer.Token(
+            start_line_number=1,
+            end_line_number=1,
+            start_column_number=9,
+            end_column_number=11,
+            identifier="ghi",
+        ),
+    )
+    self.assertEqual(
+        token3,
+        tokenizer.Token(
+            start_line_number=1,
+            end_line_number=1,
+            start_column_number=13,
+            end_column_number=15,
+            identifier="a12",
         ),
     )
 
