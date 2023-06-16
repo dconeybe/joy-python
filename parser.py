0c3d001108bfc3d4f3c6ee6503d6b162d71624ac
Denver Coneybeare <dconeybe@google.com>
2023-06-15 22:21:18 -0400

    parser.py: add ability to parse a function keyword and a function name

--- parser.py
+++ parser.py
@@ -9,9 +9,26 @@ class Parser:
 
   def __init__(self, tokenizer: tokenizer_module.Tokenizer) -> None:
     self.tokenizer = tokenizer
-    self.functions = list[JoyFunction]
+    self.functions: list[JoyFunction] = []
 
   def parse(self) -> None:
+    while True:
+      identifier = self.tokenizer.read_identifier()
+      if identifier is None:
+        assert self.tokenizer.eof()
+        break  # end-of-file
+
+      if identifier != "function":
+        raise self.ParseError(f"expected 'function' got got {identifier}")
+
+      function_name = self.tokenizer.read_identifier()
+      if identifier is None:
+        assert self.tokenizer.eof()
+        raise self.ParseError(f"expected function name after {identifier}")
+
+      self.functions.append(JoyFunction(name=function_name))
+
+  class ParseError(Exception):
     pass
 
 
--- parser_test.py
+++ parser_test.py
@@ -6,7 +6,7 @@ import parser as parser_module
 import source_reader as source_reader_module
 import tokenizer as tokenizer_module
 
-
+JoyFunction = parser_module.JoyFunction
 Parser = parser_module.Parser
 SourceReader = source_reader_module.SourceReader
 Tokenizer = tokenizer_module.Tokenizer
@@ -17,8 +17,27 @@ class ParserTest(absltest.TestCase):
   def test_init(self):
     Parser(self.create_tokenizer(""))
 
+  def test_parse_functions(self):
+    parser = self.create_parser(
+        """
+          function abc
+          function def
+          function _12
+        """
+    )
+
+    parser.parse()
+
+    self.assertEqual(
+        [JoyFunction(name="abc"), JoyFunction(name="def"), JoyFunction(name="_12")],
+        parser.functions,
+    )
+
   def create_tokenizer(self, text: str) -> Tokenizer:
     return Tokenizer(source_reader=SourceReader(io.StringIO(text)))
+
+  def create_parser(self, text: str) -> Parser:
+    return Parser(self.create_tokenizer(text))
 
 
 if __name__ == "__main__":
